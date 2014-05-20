138cdb9f6e0e90d3f9b12c52fa7bf5b7e7ffaa7d
Frank Duckhorn <frank.duckhorn@ikts-md.fraunhofer.de>
2014-05-20 09:40:58 +0200

    fix recognizer build on redhat

--- ext/portaudio/lin64/LFLAGS.sh
+++ ext/portaudio/lin64/LFLAGS.sh
@@ -1,6 +1,6 @@
 
 PA_DIR=$1
 
-DIR="/usr/lib64 /usr/lib"
+DIR="/usr/lib64 /usr/lib /lib /lib64"
 AS=`find $DIR -maxdepth 2 -name 'libasound.so*' | head -n1`
 echo $PA_DIR"/libportaudio.a $AS -lrt -ldl"
