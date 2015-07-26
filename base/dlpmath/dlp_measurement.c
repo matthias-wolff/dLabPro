94f5f0362d339d9d473b9bf574459ed9f2d508e0
Arnulf Becker <Arnulf.Becker@ikts-md.fraunhofer.de>
2015-07-26 22:03:37 +0200

    Changed approach - keep floating and fixed point in parallel. Fixed measurment.

--- base/dlpmath/dlp_measurement.c
+++ base/dlpmath/dlp_measurement.c
@@ -32,7 +32,7 @@ void data2csv_INT16(DataLog* log, const char* id, INT16* input, INT32 dim) {
 	INT32 i;
 	fprintf(log->file, "#<---I16---%s--->\n", id);
 	for (i = 0; i < dim; i++) {
-		fprintf(log->file, "%d,%08x,%d,\n", log->line++, input[i], input[i]);
+		fprintf(log->file, "%d,%08x,%d\n", log->line++, input[i], input[i]);
 	}
 }
 
