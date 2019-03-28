5a3c43079d984b1162509922e8bd0462c99ce4b5
Frank Duckhorn <git@bitboulder.net>
2019-03-28 08:47:18 +0100

    fix memory error in numpy2data for no compact array

--- programs/python/dlabpro_numpy.cpp
+++ programs/python/dlabpro_numpy.cpp
@@ -40,12 +40,7 @@ void numpy2data(PyObject *np,CData *dat){
   d=na->strides[na->nd-1];
   for(i=na->nd-1;i;i--){
     d=d*na->dimensions[i];
-    if(na->strides[i-1]!=d){
-      np=PyArray_Copy(na);
-      numpy2data(np,dat);
-      PyArray_free(np);
-      return;
-    }
+    if(na->strides[i-1]!=d){ printf("ERROR: no compact array"); return; }
   }
   CData_Array(dat,t,c,r);
   CData_SetNBlocks(dat,b);
