5ae79197bd92d96329254e033cb7ed33ac01ba54
Frank Duckhorn <frank.duckhorn@ikts-md.fraunhofer.de>
2020-05-05 08:39:02 +0200

    fix compile error on newisland

--- ext/portaudio/lin64/LFLAGS.sh
+++ ext/portaudio/lin64/LFLAGS.sh
@@ -3,5 +3,8 @@ PA_DIR=$1
 
 DIR="/usr/lib64 /usr/lib /lib /lib64 /usr/lib/x86_64-linux-gnu"
 AS=`find $DIR -maxdepth 2 -name 'libasound.so*' | head -n1`
-#echo $PA_DIR"/libportaudio.a $AS -lrt -ldl"
-echo "-lportaudio"
+if gcc -lportaudio 2>&1 | grep -q "cannot find -lportaudio"; then
+  echo $PA_DIR"/libportaudio.a $AS -lrt -ldl"
+else
+  echo "-lportaudio"
+fi
